a4a186683418453631b5bc17cc9867596bae6765
QQ <cchangwo@yahoo.com.tw>
2020-12-21 01:08:16 +0800

    Icons for rating
    install command:
    yarn add react-native-svg @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-native-fontawesome

--- componment/detail.js
+++ componment/detail.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { FlatList, Platform, StyleSheet, Text, View,Image } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Detail (props) {
 
@@ -8,7 +10,16 @@ export default function Detail (props) {
 
     return (
         <View >
-            <Text>Details about {movie.title} </Text>
+            <Text>{movie.title} </Text>
+            <View style={styles.starConatiner}>
+                <FontAwesomeIcon style={movie.avg_rating > 0? styles.orange : styles.white} icon={faStar}/>
+                <FontAwesomeIcon style={movie.avg_rating > 1? styles.orange : styles.white} icon={faStar}/>
+                <FontAwesomeIcon style={movie.avg_rating > 2? styles.orange : styles.white} icon={faStar}/>
+                <FontAwesomeIcon style={movie.avg_rating > 3? styles.orange : styles.white} icon={faStar}/>
+                <FontAwesomeIcon style={movie.avg_rating > 4? styles.orange : styles.white} icon={faStar}/>
+                <Text>({movie.no_of_ratings})</Text>
+           </View>
+            <Text>{movie.description} </Text>
         </View>
     );
 }
@@ -29,5 +40,16 @@ const styles = StyleSheet.create({
   itemText:{
     color: "#fff",
     fontSize: 24
+  },
+  starConatiner:{
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection:"row"
+  },
+  orange:{
+      color: "orange"
+  },
+  white:{
+      color: "black"
   }
 });
